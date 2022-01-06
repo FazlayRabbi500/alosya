80e1b6b9e0289d198d9025b463859c7faec8e5f4
Fazlay Rabbi <89375452+FazlayRabbi500@users.noreply.github.com>
2022-01-06 20:18:49 +0600

    Add files via upload

---
+++ assets/js/active.js
@@ -0,0 +1,21 @@
+
+(function($){
+	"use strict"
+	jQuery(document).ready(function($){
+		 
+
+         $("ul#navigation").slicknav({
+			prependTo: ".responsive-menu-wrap"
+		});
+
+		new WOW().init();
+
+
+	});
+
+	
+
+
+
+
+}(jQuery));
